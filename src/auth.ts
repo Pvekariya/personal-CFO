f80e8d823097a6e7d72ecebfd1c146eea483acb9
PRATIK VEKARIYA <psvekariya092@gmail.com>
2026-06-04 12:55:59 +0530

    fix: remove syntax error in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -6,7 +6,7 @@ import { loginSchema } from "@/lib/validations/auth"
 
 // Force correct URLs on Vercel deployments, ignoring bad environment variables
 if (process.env.VERCEL_URL || process.env.VERCEL_PROJECT_PRODUCTION_URL) {
-  const url = \`https://\${process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL}\`
+  const url = `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL || process.env.VERCEL_URL}`
   process.env.AUTH_URL = url
   process.env.NEXTAUTH_URL = url
 }
